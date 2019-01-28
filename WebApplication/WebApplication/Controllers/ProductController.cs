a4487e3afcdfbc38ce32e53214fe0b9c3221adbd
paulpozar <42919065+paulpozar@users.noreply.github.com>
2019-01-27 20:14:41 -0500

    Lab 02

---
+++ WebApplication/WebApplication/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Public/Index is displayed";
+        }
+
+        // GET: Product/Browse
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+
+        // GET: Product/Details/105
+        public string Details(int id)
+        {
+            string message = "Details displayed for Id=" + id;
+            return message;
+        }
+
+        // GET: Product/Location?zip=?44124
+        public string Location(int zip)
+        {
+            string message = HttpUtility.HtmlEncode("Location displayed for zip=" + zip);
+            return message;
+        }
+    }
+}
