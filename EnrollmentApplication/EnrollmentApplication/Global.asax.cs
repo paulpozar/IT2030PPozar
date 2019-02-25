82b2c0d6be59306004b8ac3408cf0768fdc389fb
paulpozar <42919065+paulpozar@users.noreply.github.com>
2019-02-24 22:51:16 -0500

    Lab 5

--- EnrollmentApplication/EnrollmentApplication/Global.asax.cs
+++ EnrollmentApplication/EnrollmentApplication/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using EnrollmentApplication.Models;
 
 namespace EnrollmentApplication
 {
@@ -12,6 +14,7 @@ namespace EnrollmentApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EnrollmentDBContext>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
