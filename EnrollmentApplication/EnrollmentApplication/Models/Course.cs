bec484d32d70fae52cadd2cbe25d32e63eec7b7b
paulpozar <42919065+paulpozar@users.noreply.github.com>
2019-03-12 21:32:03 -0400

    Lab 6

--- EnrollmentApplication/EnrollmentApplication/Models/Course.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,20 @@ namespace EnrollmentApplication.Models
 {
     public class Course
     {
+        [Display(Name="Course ID")]
         public virtual int CourseID { get; set; }
+
+        [Required]
+        [Display(Name="Course Title")]
+        [StringLength(150)]
         public virtual string Title { get; set; }
+
+        [Display(Name ="Description")]
         public virtual string Description { get; set; }
+
+        [Required]
+        [Display(Name ="Number of credits")]
+        [Range(1,4)]
         public virtual int Credits { get; set; }
     }
 }
--- EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,12 +11,24 @@ namespace EnrollmentApplication.Models
         public virtual int EnrollmentID { get; set; }
         public virtual int StudentID { get; set; }
         public virtual int CourseID { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-F]")]
         public virtual char Grade { get; set; }
         public virtual Student Student { get; set; }
         public virtual Course Course { get; set; }
         public virtual bool IsActive { get; set; }
+
+        [Required]
+        [Display(Name ="Assigned Campus")]
         public virtual string AssignedCampus { get; set; }
+
+        [Required]
+        [Display(Name = "Enrolled in Semester")]
         public virtual string EnrollmentSemester { get; set; }
+
+        [Required]
+        [Range(2018, 9999)]
         public virtual int EnrollmentYear { get; set; }
     }
 }
